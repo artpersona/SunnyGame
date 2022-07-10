510db99302bc3035e37dbe5f74378545c2c001af
John Paul Perez <jp11perez@gmail.com>
2022-07-10 16:59:44 +0800

    Initial Commit

---
+++ App.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Linking,
+  TouchableOpacity,
+  StatusBar,
+  Image,
+  Dimensions,
+  Alert,
+} from 'react-native';
+import {RFValue} from 'react-native-responsive-fontsize';
+import LinearGradient from 'react-native-linear-gradient';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.header__text}>Sunny Game</Text>
+      </View>
+
+      <View style={styles.image__container}>
+        <Image
+          source={require('./assets/icon.png')}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      </View>
+      <TouchableOpacity
+        onPress={
+          () => Linking.openURL('http://store.lgsunny.com/new/reg/408753')
+          // Alert.alert('Clicked!')
+        }
+        style={{width: '80%'}}>
+        <LinearGradient
+          // Button Linear Gradient
+          colors={['#7e020f', '#b5001c', '#740c02']}
+          style={styles.button}>
+          <Text style={styles.button__text}>PLAY NOW</Text>
+        </LinearGradient>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() =>
+          Linking.openURL('https://www.facebook.com/dreamclubofficialpage')
+        }
+        style={{width: '80%'}}>
+        <LinearGradient
+          // Button Linear Gradient
+          colors={['#0300ff', '#1071ff', '#2a0096']}
+          style={styles.button}>
+          <Text style={styles.button__text}>Customer Service</Text>
+        </LinearGradient>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+let deviceWidth = Dimensions.get('window').width;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+  },
+  header: {
+    backgroundColor: '#212121',
+    width: '100%',
+    height: 65,
+    justifyContent: 'center',
+  },
+  header__text: {
+    color: 'white',
+    fontSize: RFValue(19),
+    marginLeft: '5%',
+    fontWeight: 'bold',
+  },
+
+  image__container: {
+    width: deviceWidth / 1.3,
+    height: deviceWidth / 1.3,
+    marginTop: '10%',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+
+  button: {
+    height: 80,
+    backgroundColor: 'white',
+    borderRadius: RFValue(10),
+    marginTop: '20%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  button__text: {
+    fontSize: RFValue(20),
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
